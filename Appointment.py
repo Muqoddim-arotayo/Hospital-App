a2719f4fe2151146dd306b64672d807468afc21b
Muqoddim-arotayo <112625006+Muqoddim-arotayo@users.noreply.github.com>
2025-10-08 23:31:22 +0100

    updated my OOP

--- Appointment.py
+++ Appointment.py
@@ -2,7 +2,7 @@ def book_appointment():
     print("Book appointment")
 
 def cancel_appointment():
-    print("cancel appointment")
+    print("cancel appointment")    
 
 def check_pending_appointments():
     print("Pending appointmennt")
